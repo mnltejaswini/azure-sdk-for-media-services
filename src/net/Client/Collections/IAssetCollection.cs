b8d6a6553341a8a14ab811fcd5659dc4ab81a807
mnltejaswini <mnl.tejaswini@gmail.com>
2014-06-28 00:30:48 -0700

    Revert "Added comments to the methods in the interfaces"
    This reverts commit 2fde602c44b376c1dfd20d9c2a7e3f80de92c8a9.

--- src/net/Client/Collections/IAssetCollection.cs
+++ src/net/Client/Collections/IAssetCollection.cs
@@ -1,70 +1,20 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="IAssetCollection.cs" company="Microsoft">Copyright 2012 Microsoft Corporation</copyright>
-// <license>
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-// http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </license>
-
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Text;
 
 namespace Microsoft.WindowsAzure.MediaServices.Client
 {
-    /// <summary>
-    /// Represents an interface for Asset collection of <see cref="IAsset"/>.
-    /// </summary>
     public interface IAssetCollection : IQueryable<IAsset>
     {
-        /// <summary>
-        /// Asynchronously creates an asset that does not contain any files and <see cref="AssetState"/> is Initialized.
-        /// </summary>
-        /// <param name="assetName">The asset name.</param>
-        /// <param name="options">A <see cref="AssetCreationOptions"/> which will be associated with created asset.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>
-        /// An <see cref="Task"/> of type <see cref="IAsset"/>created according to the specified creation <paramref name="options"/>.
-        /// </returns>
         Task<IAsset> CreateAsync(string assetName, AssetCreationOptions options, CancellationToken cancellationToken);
 
-        /// <summary>
-        /// Creates an asset that does not contain any files and <see cref="AssetState"/> is Initialized. 
-        /// </summary>
-        /// <param name="assetName">The asset name.</param>
-        /// <param name="options">A <see cref="AssetCreationOptions"/> which will be associated with created asset.</param>
-        /// <returns>The created asset.</returns>
         IAsset Create(string assetName, AssetCreationOptions options);
 
-        /// <summary>
-        /// Asynchronously creates an asset for specified storage account. Asset  does not contain any files and <see cref="AssetState" /> is Initialized.
-        /// </summary>
-        /// <param name="assetName">The asset name.</param>
-        /// <param name="storageAccountName">The storage account name</param>
-        /// <param name="options">A <see cref="AssetCreationOptions" /> which will be associated with created asset.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>
-        /// An <see cref="Task" /> of type <see cref="IAsset" />, where IAsset created according to the specified creation <paramref name="options" />.
-        /// </returns>
         Task<IAsset> CreateAsync(string assetName, string storageAccountName, AssetCreationOptions options, CancellationToken cancellationToken);
 
-        /// <summary>
-        /// Creates an asset for specified storage account. Asset does not contain any files and <see cref="AssetState" /> is Initialized.
-        /// </summary>
-        /// <param name="assetName">The asset name.</param>
-        /// <param name="storageAccountName"></param>
-        /// <param name="options">A <see cref="AssetCreationOptions" /> which will be associated with created asset.</param>
-        /// <returns>
-        /// The created asset.
-        /// </returns>
         IAsset Create(string assetName, string storageAccountName, AssetCreationOptions options);
     }
 }
--- src/net/Client/Collections/ILocatorCollection.cs
+++ src/net/Client/Collections/ILocatorCollection.cs
@@ -1,106 +1,20 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="ILocatorCollection.cs" company="Microsoft">Copyright 2012 Microsoft Corporation</copyright>
-// <license>
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-// http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </license>
-
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace Microsoft.WindowsAzure.MediaServices.Client
 {
-    /// <summary>
-    /// Represents an interface for collection of <see cref="ILocator"/>.
-    /// </summary>
     public interface ILocatorCollection : IQueryable<ILocator>
     {
-        /// <summary>
-        /// Asynchronously creates a SAS Locator with the specified access policy and asset.
-        /// </summary>
-        /// <param name="asset">The asset to create a SAS Locator for.</param>
-        /// <param name="accessPolicy">The AccessPolicy that governs access for the locator.</param>
-        /// <returns>A function delegate that returns the future result to be available through the Task&lt;ILocator&gt;.</returns>
-        /// <exception cref="ArgumentNullException">When <paramref name="asset"/> is null.</exception>
-        /// <exception cref="ArgumentNullException">When <paramref name="accessPolicy"/> is null.</exception>
         Task<ILocator> CreateSasLocatorAsync(IAsset asset, IAccessPolicy accessPolicy);
-
-        /// <summary>
-        /// Creates a SAS Locator with the specified access policy and asset.
-        /// </summary>
-        /// <param name="asset">The asset to create a SAS Locator for.</param>
-        /// <param name="accessPolicy">The AccessPolicy that governs access for the locator.</param>
-        /// <returns>A locator granting access specified by <paramref name="accessPolicy" /> to the provided <paramref name="asset" />.</returns>
-        /// <exception cref="ArgumentNullException">When <paramref name="asset"/> is null.</exception>
-        /// <exception cref="ArgumentNullException">When <paramref name="accessPolicy"/> is null.</exception>
         ILocator CreateSasLocator(IAsset asset, IAccessPolicy accessPolicy);
-
-        /// <summary>
-        /// Creates a SAS Locator with the specified access policy and asset.
-        /// </summary>
-        /// <param name="asset">The asset to create a SAS Locator for.</param>
-        /// <param name="accessPolicy">The AccessPolicy that governs access for the locator.</param>
-        /// <param name="startTime">The access start time of the locator.</param>
-        /// <param name="name">The locator name.</param>
-        /// <returns>
-        /// A locator granting access specified by <paramref name="accessPolicy" /> to the provided <paramref name="asset" />.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">When <paramref name="asset" /> is null.</exception>
-        /// <exception cref="ArgumentNullException">When <paramref name="asset" /> is null.</exception>
         ILocator CreateSasLocator(IAsset asset, IAccessPolicy accessPolicy, DateTime? startTime, string name = null);
-        
-        /// <summary>
-        /// Asynchronously creates a SAS Locator with the specified access policy and asset.
-        /// </summary>
-        /// <param name="asset">The asset to create a SAS Locator for.</param>
-        /// <param name="accessPolicy">The AccessPolicy that governs access for the locator.</param>
-        /// <param name="startTime">The access start time of the locator.</param>
-        /// <param name="name">The locator name.</param>
-        /// <returns>
-        /// A function delegate that returns the future result to be available through the Task&lt;ILocator&gt;.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">When <paramref name="asset" /> is null.</exception>
-        /// <exception cref="ArgumentNullException">When <paramref name="asset" /> is null.</exception>
         Task<ILocator> CreateSasLocatorAsync(IAsset asset, IAccessPolicy accessPolicy, DateTime? startTime, string name = null);
-
-        /// <summary>
-        /// Creates the locator async.
-        /// </summary>
-        /// <param name="locatorType">Type of the locator.</param>
-        /// <param name="asset">The asset.</param>
-        /// <param name="accessPolicy">The access policy.</param>
-        /// <param name="startTime">The start time.</param>
-        /// <param name="name">The name.</param>
-        /// <returns>
-        /// A function delegate that returns the future result to be available through the Task&lt;ILocator&gt;.
-        /// </returns>
         Task<ILocator> CreateLocatorAsync(LocatorType locatorType, IAsset asset, IAccessPolicy accessPolicy, DateTime? startTime, string name = null);
-        
         Task<ILocator> CreateLocatorAsync(LocatorType locatorType, IAsset asset, IAccessPolicy accessPolicy);
-
-        /// <summary>
-        /// Creates the locator.
-        /// </summary>
-        /// <param name="locatorType">Type of the locator.</param>
-        /// <param name="asset">The asset.</param>
-        /// <param name="accessPolicy">The access policy.</param>
-        /// <param name="startTime">The start time.</param>
-        /// <param name="name">The name.</param>
-        /// <returns>
-        /// A locator enabling streaming access to the specified <paramref name="asset" />.
-        /// </returns>
         ILocator CreateLocator(LocatorType locatorType, IAsset asset, IAccessPolicy accessPolicy, DateTime? startTime, string name = null);
-        
         ILocator CreateLocator(LocatorType locatorType, IAsset asset, IAccessPolicy accessPolicy);
 
     }
